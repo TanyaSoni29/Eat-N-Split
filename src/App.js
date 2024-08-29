d1172007ca309e4af12b51039ad4c611bf419517
TanyaSoni29 <tanu139sona@gmail.com>
2024-08-29 23:07:55 +0530

    Imp. of key prop in Changing the State

--- src/App.js
+++ src/App.js
@@ -62,6 +62,7 @@ function App() {
       </div>
       {selectedFriend && (
         <BillSplitCalci
+        key={selectedFriend.id}
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
         />
